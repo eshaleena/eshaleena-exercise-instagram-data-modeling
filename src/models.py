efc1b6895f373e27da310bd994a3b709b396e6c8
Shaleena Evans <123431776+eshaleena@users.noreply.github.com>
2023-08-07 21:06:58 +0000

    post and media

--- src/models.py
+++ src/models.py
@@ -7,12 +7,14 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+
 class Follower(Base):
     __tablename__ = 'follower'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     user_to_id = Column(Integer, ForeignKey('user.id'))
+
 
 class User(Base):
     __tablename__ = 'user'
@@ -24,10 +26,21 @@ class User(Base):
     last_name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
 
-    def to_dict(self):
-        return {}
+    class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
-## Draw from SQLAlchemy base
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(200), unique=True)
+    url = Column(String(200), unique=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+
+# Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
