9a8cb92b77cb128f64f1556301e6dfcf405768e3
Shaleena Evans <123431776+eshaleena@users.noreply.github.com>
2023-08-07 21:12:23 +0000

    another edit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,24 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 
-class Follower(Base):
-    __tablename__ = 'follower'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    user_to_id = Column(Integer, ForeignKey('user.id'))
-
-
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    user_name = Column(String(250), nullable=False)
-    first_name = Column(String(250), nullable=False)
-    last_name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    first_name = Column(String(250))
+    last_name = Column(String(250))
     email = Column(String(250), nullable=False)
+    posts = relationship('Post', backref='user')
+    comments = relationship('Comment', backref='user')
+    followers = relationship('Follower', backref='user')
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    url = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
 
 class Post(Base):
     __tablename__ = 'post'
