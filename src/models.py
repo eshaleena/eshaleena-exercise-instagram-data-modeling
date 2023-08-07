5e18758d476acb52709bee1d4151fed4a33bbac0
Shaleena Evans <123431776+eshaleena@users.noreply.github.com>
2023-08-07 21:03:28 +0000

    add follower

--- src/models.py
+++ src/models.py
@@ -7,12 +7,12 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Follower(Base):
+    __tablename__ = 'follower'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'))
 
 class Address(Base):
     __tablename__ = 'address'
