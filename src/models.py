43f3f33db8587b584e823ee196b681f194ee2c06
Shaleena Evans <123431776+eshaleena@users.noreply.github.com>
2023-08-07 22:19:49 +0000

    done

--- src/models.py
+++ src/models.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
-Base = declarative_base()
 
+Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
@@ -20,6 +20,13 @@ class User(Base):
     followers = relationship('Follower', backref='user')
 
 
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    media = relationship('Media', backref='post')
+    comments = relationship('Comment', backref='post')
+
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
@@ -27,18 +34,6 @@ class Media(Base):
     url = Column(String(250))
     post_id = Column(Integer, ForeignKey('post.id'))
 
-class Post(Base):
-    __tablename__ = 'post'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-
-
-class Media(Base):
-    __tablename__ = 'media'
-    id = Column(Integer, primary_key=True)
-    type = Column(String(200), unique=True)
-    url = Column(String(200), unique=True)
-    post_id = Column(Integer, ForeignKey('post.id'))
 
 class Comment(Base):
     __tablename__ = 'comment'
@@ -47,14 +42,16 @@ class Comment(Base):
     author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
 
-    def to_dict(self):
-        return {}
-
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
 
 # Draw from SQLAlchemy base
 try:
-    result = render_er(Base, 'diagram.png')
+    render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
